5917659ace12eec2fb25c9df2ae2a7d07074dce4
pepedinho <2spii94@gmail.com>
2025-03-22 13:58:07 +0100

    start react implementation exercice

--- wallet/solana-course/src/App.tsx
+++ wallet/solana-course/src/App.tsx
@@ -3,17 +3,29 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { clusterApiUrl } from "@solana/web3.js";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import {
+  clusterApiUrl,
+  Transaction,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const Home = () => {
   const endpoint = clusterApiUrl("devnet");
   const wallets = useMemo(() => [], []);
-  
+
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets}>
-        <p>Put the rest of your app here</p>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <p>Put the rest of your app here</p>
+        </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
